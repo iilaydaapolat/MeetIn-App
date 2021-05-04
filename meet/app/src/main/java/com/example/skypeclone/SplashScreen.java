7cdf221f72f2ca2bbb79bd098639d8f848d3befb
iilaydaapolat <82779853+iilaydaapolat@users.noreply.github.com>
2021-05-05 00:55:42 +0300

    MeetIn App
    Üniverisite tercihini gerçekleştirek olan öğrencilerin
     Hasan Kalyoncu Universitesi öğretmenleriyle
    bölüm ve üniversite hakkında bilgi alma amaçlı yapacağı online görüntülü sohbet uygulaması.

---
+++ meet/app/src/main/java/com/example/skypeclone/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.example.skypeclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    Intent intent = new Intent(SplashScreen.this, RegistrationActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+
+    }
+}
